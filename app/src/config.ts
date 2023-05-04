1fe7cdf7d69bfd458f6d6dca1e43037635ca074d
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2023-05-04 22:52:40 +0200

    app: Update player config

--- app/src/config.ts
+++ app/src/config.ts
@@ -13,8 +13,8 @@ export const SERVER_URL = import.meta.env.VITE_SERVER_URL;
 export const FPS = 60;
 
 export const PLAYER_CONFIG = {
-    speed: 2,
-    acceleration: 0.5,
-    deceleration: 0.1,
+    speed: 10,
+    acceleration: 2.0,
+    deceleration: 1.0,
     attackDelayMs: 1000,
 };
