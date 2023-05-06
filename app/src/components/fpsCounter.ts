1b621202914723087f1154cf06315f0540702c7b
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2023-05-06 17:53:14 +0200

    app: Show fps average every 500ms

--- app/src/components/fpsCounter.ts
+++ app/src/components/fpsCounter.ts
@@ -1,12 +1,23 @@
+import { createTimer, Timer } from "timesub";
 import { System } from "../systems";
 
 export class FpsCounter implements System {
     private lastUpdate: Date;
     private fpsEl: HTMLElement;
+    private timer: Timer;
+    private fpsNumbers: number[];
 
     constructor() {
         this.lastUpdate = new Date();
         this.fpsEl = document.querySelector(".fps")!;
+        this.timer = createTimer({
+            duration: "infinite",
+            updateInterval: 500,
+        });
+        this.fpsNumbers = [];
+
+        this.timer.play();
+        this.timer.on("update", this.displayAverage.bind(this));
 
         if (!this.fpsEl) {
             throw new Error("[FpsCounter] Expected .fps element");
@@ -17,8 +28,15 @@ export class FpsCounter implements System {
         const now = new Date();
 
         const fps = 1000 / (now.getTime() - this.lastUpdate.getTime());
-        this.fpsEl.innerText = fps.toString().padStart(2, "0");
+        this.fpsNumbers.push(fps);
 
         this.lastUpdate = now;
+    }
+
+    private displayAverage() {
+        const avg =
+            this.fpsNumbers.reduce((a, b) => a + b, 0) / this.fpsNumbers.length;
+        this.fpsEl.innerText = avg.toFixed(2).padStart(2, "0");
+        this.fpsNumbers = [];
     }
 }
